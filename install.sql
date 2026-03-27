cfcf7941335984851f174e8965fe35ee272acb52
braveua <braveua@gmail.com>
2026-03-27 21:20:05 +0200

    new file:   TABLES/SH_PROD_HIST.sql 	new file:   TRIGGERS/TRG_SH_PROD_LOG.sql 	new file:   install.sql

---
+++ install.sql
@@ -0,0 +1,23 @@
+-- 1. users / roles
+@@ROLES/SHOPROLERO.sql
+@@ROLES/SHOP_WRITER_ROLE.sql
+
+-- 2. tables
+@@TABLES/SH_SHOP.sql
+@@TABLES/SH_PROD.sql
+
+-- 3. sequences
+@@SEQUENCES/SH_SHOPID.sql
+
+-- 4. indexes
+@@INDEXES/UNIQUE_DATE_PROD.sql
+
+-- 5. packages
+@@PKG/SH.pks
+@@PKG/SH.pkb
+
+-- 6. views
+@@VIEWS/VPRODUCT.sql
+
+-- 7. privileges
+@@PRIVILEGES/object_privileges.sql
